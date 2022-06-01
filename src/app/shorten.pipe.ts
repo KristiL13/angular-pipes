d455a88e50d7ac61a67f65aa4bd9d95566f5209b
Kristi Laur <kristi@heisi.eu>
2022-06-01 13:21:40 +0300

    Add parameter to my own pipe

--- src/app/shorten.pipe.ts
+++ src/app/shorten.pipe.ts
@@ -4,11 +4,11 @@ import { Pipe, PipeTransform } from "@angular/core";
   name: 'shorten'
 })
 export class ShortenPipe implements PipeTransform {
-  transform(value: any /*, ...args: any[]*/) {
-    if (value.length > 13) {
+  transform(value: any, limit: number /*, anotherArg: any*/) {
+    if (value.length > limit) {
       console.log(value.length);
       // transform always needs to return something
-      return value.substr(0, 13) + ' ...';
+      return value.substr(0, limit) + ' ...';
     }
     return value;
   }
